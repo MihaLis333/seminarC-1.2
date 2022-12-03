a741b53f33d0a884c63967213176e4025b6caf83
MihaLis333 <m190333@ya.ru>
2022-12-03 16:39:02 +0300

    программа удаления среднего числа из трехначного числа

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int CutNumber(int num)
+{
+    int ed = num % 10;
+    int sot = num / 100;
+
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int rundNum = new Random().Next(100, 1000);
+int newNum = CutNumber(rundNum);
+
+Console.WriteLine($"Новая версия числа {rundNum} это {newNum}");
